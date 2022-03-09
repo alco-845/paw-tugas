dd288eceafbe242f41c014cb7c879e0f1eb1648b
alco2845 <nicola.alivant@gmail.com>
2022-03-09 15:33:58 +0700

    menabahkan operasi matematika menggunakan 2 variable di variable.php

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php 
+
+$bil1 = 10;
+$bil2 = 7;
+
+$tambah = $bil1 + $bil2;
+echo $bil1 . ' + ' . $bil2 . " = " . $tambah;
+
+$kurang = $bil1 - $bil2;
+echo '<br>' . $bil1 . ' - ' . $bil2 . " = " . $kurang;
+
+$kali = $bil1 * $bil2;
+echo '<br>' . $bil1 . ' * ' . $bil2 . " = " . $kali;
+
+?>
